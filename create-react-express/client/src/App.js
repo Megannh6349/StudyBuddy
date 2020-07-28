d5b8c0ea95621ffc84d0df37ec3a064ea11f7919
KunZizinho <kun.zizinho@gmail.com>
2020-07-28 18:55:49 -0400

    adding depend

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -1,3 +1,24 @@
+//requ our servers 
+const express = require("express");
+// Helmet helps you secure your Express apps by setting various HTTP headers.
+const helmet = require("helmet");
+// Cross-origin resource sharing (CORS)
+const cors = require("cors");
+// database mongo db
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+
+require("dotenv").config();
+const port = process.env.PORT || 3001;
+const mongoDB =
+  process.env.NODE_ENV !== "test"
+    ? process.env.MONGODB
+    : process.env.MONGODBTEST;
+
+
+require('dotenv').config();
+const routes = require('./users/routes')
 import React, { Component } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
